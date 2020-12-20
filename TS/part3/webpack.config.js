d4f22bf3d842bf539f262621c8315342852c943d
975795875@qq.com <975795875@qq.com>
2020-12-20 20:26:59 +0800

    TS webpack打包 JS 原型链 扩展内置对象

---
+++ TS/part3/webpack.config.js
@@ -0,0 +1,31 @@
+//引入一个包
+const path = require('path');
+
+//webpack中所有的配置信息都应该写在modules中
+module.exports = {
+  //指定入口文件
+  entry:"./src/index.ts",
+
+  //指定打包文件所在的目录
+  output: {
+    //指定打包后的目录
+    path: path.resolve(__dirname,'dist'),
+    //打包后文件的名字
+    filename:"bundle.js"
+  },
+
+  //指定webpack打包时要使用模块
+  module:{
+    //指定要加载的规则
+    rules:[
+      {
+        //test指定的是规则生效的文件
+        test:/\.ts$/,
+        //要使用loader
+        use:'ts-loader',
+        //要排除的文件
+        exclude:/node_modules/
+      }
+    ]
+  }
+};
