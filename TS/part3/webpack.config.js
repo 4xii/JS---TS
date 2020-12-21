af7cdd44afd15d07514890abe60b23ebb0e1af86
975795875@qq.com <975795875@qq.com>
2020-12-21 22:34:28 +0800

    JS 数组 TS webpack

--- TS/part3/webpack.config.js
+++ TS/part3/webpack.config.js
@@ -1,6 +1,9 @@
 //引入一个包
 const path = require('path');
-
+//引入html插件
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+//引入clean插件
+const { CleanWebpackPlugin} = require('clean-webpack-plugin');
 //webpack中所有的配置信息都应该写在modules中
 module.exports = {
   //指定入口文件
@@ -27,5 +30,19 @@ module.exports = {
         exclude:/node_modules/
       }
     ]
+  },
+
+  //配置webpack插件
+  plugins:[
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin( {
+      //title:"这是一个自定义的title"
+      template: "./src/index.html"
+    }),
+  ],
+
+  //用来设置引用模块
+  resolve: {
+    extensions:['.ts','.js']
   }
 };
